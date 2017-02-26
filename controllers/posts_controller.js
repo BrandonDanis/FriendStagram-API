201ce95a178832b212d1ebca03b6e18ced37977f
Kyle Kung <kylekung@hotmail.com>
2017-02-25 22:55:06 -0500

    Added error handling to prevent from inserting posts without url

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -5,16 +5,35 @@ const qs = require('querystring')
 
 module.exports.addPosts = (req, res) => {
     var userID = req.user.id
-    post.addPosts(req.body.description, req.body.url, req.body.tags, req.user.id, function (err, data) {
-        if(err)
-            return res.status(400).json({error: true, data: null})
-        user.linkPosts(userID, data, function (err, data) {
-            res.status(err ? 404 : 200).json({
-                error: err,
-                data: data
+    var url = req.body.url
+
+    var errorMessage = ""
+
+    if (utils.isEmpty(userID)) {
+        errorMessage+="User ID is Null\n"
+    }
+    if (utils.isEmpty(url)) {
+        errorMessage+="URL is Null\n"
+    }
+
+    if(!utils.isEmpty(errorMessage)){
+        res.status(401).json({
+            error: true,
+            data: errorMessage
+        })
+    }
+    else {
+        post.addPosts(req.body.description, req.body.url, req.body.tags, req.user.id, function (err, data) {
+            if(err)
+                return res.status(400).json({error: true, data: null})
+            user.linkPosts(userID, data, function (err, data) {
+                res.status(err ? 404 : 200).json({
+                    error: err,
+                    data: data
+                })
             })
         })
-    })
+    }
 }
 
 module.exports.getPostsByUser = (req, res) => {

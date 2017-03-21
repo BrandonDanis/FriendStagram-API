006117887617db6c19bcd86d9c9a3d8a9d2e0615
Brandon Danis <brandon@spottedu.co>
2017-03-21 11:34:00 -0400

    Removed some routes for now

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -6,6 +6,8 @@ const auth = require("../auth.js")
     app.post('/', auth.authenticate, postsController.addPosts)
 
     app.get('/', postsController.search)
+    //app.get('/feed', postsController.getFollowingPosts)
+    //app.get('/public', postsController.getPublicPosts)
     app.get('/id/:postid', postsController.getPostByID)
     app.get('/user/:username',postsController.getPostsByUser)
 

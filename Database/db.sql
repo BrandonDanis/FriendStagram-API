9a93081559bf0e7b74f8ee6e4c3434263570152c
Brandon Danis <brandon@spottedu.co>
2017-03-20 22:56:10 -0400

    updated schema

--- Database/db.sql
+++ Database/db.sql
@@ -1,38 +1,28 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  id                    SERIAL,
-  name                  VARCHAR(35),
-  username              VARCHAR(25)  NOT NULL UNIQUE,
-  password              VARCHAR(100) NOT NULL,
-  datecreated           TIMESTAMP    NOT NULL DEFAULT (NOW()),
-  email                 VARCHAR(254) NOT NULL UNIQUE,
-  description           VARCHAR(250),
-  dailyLimit            INTEGER,
-  profile_picture_id    INTEGER,
-  profile_background_id INTEGER,
-  verified              BOOLEAN      NOT NULL DEFAULT FALSE,
+  id                        SERIAL,
+  name                      VARCHAR(35),
+  username                  VARCHAR(25)  NOT NULL UNIQUE,
+  password                  VARCHAR(100) NOT NULL,
+  datecreated               TIMESTAMP    NOT NULL DEFAULT (NOW()),
+  email                     VARCHAR(254) NOT NULL UNIQUE,
+  description               VARCHAR(250),
+  dailyLimit                INTEGER,
+  profile_picture_url       VARCHAR(250),
+  profile_background_url    VARCHAR(250),
+  verified                  BOOLEAN      NOT NULL DEFAULT FALSE,
   UNIQUE (username, email),
-  FOREIGN KEY (profile_picture_id) REFERENCES images (id),
-  FOREIGN KEY (profile_background_id) REFERENCES images (id),
-  PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS images;
-CREATE TABLE images (
-  id        SERIAL,
-  publicKey VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
   id          SERIAL,
-  image_id    INTEGER,
   description VARCHAR(250),
   user_id     INTEGER,
   tag_id      INTEGER,
+  image_url   VARCHAR(255) NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (image_id) REFERENCES images (id),
   FOREIGN KEY (tag_id) REFERENCES tags (id),
   PRIMARY KEY (id)
 );

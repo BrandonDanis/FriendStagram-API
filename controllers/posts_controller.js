75ae326484b79a15f521673f05c1234d73148441
Rushil Perera <pererarushil1940@gmail.com>
2017-09-30 12:34:06 -0400

    #18 Fixed missing response and duplicate usage of post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,8 +1,8 @@
-const post = require('../model/posts_model');
+const postModel = require('../model/posts_model');
 const utils = require('../utils/util');
 
-module.exports.addPosts = async ({body: {url = null, description = null, tags = null}, user = null}, res) => {
-    let errors = [];
+module.exports.addPosts = async ({ body: { url = null, description = null, tags = null }, user = null }, res) => {
+    const errors = [];
 
     if (utils.isEmpty(user.id)) {
         errors.push('User ID is Null');
@@ -14,85 +14,93 @@ module.exports.addPosts = async ({body: {url = null, description = null, tags = 
     if (errors.length > 0) {
         res.status(400).json({
             error: true,
-            data: errors.join('\n')
-        })
+            data: errors.join('\n'),
+        });
     } else {
         try {
-            const post = await post.addPosts(description, url, tags, user.id);
+            const post = await postModel.addPosts(description, url, tags, user.id);
             res.status(200).json({
                 error: false,
-                data: post
+                data: post,
             });
         } catch (e) {
             console.error(e);
             res.status(500).json({
                 error: true,
-                msg: 'Failed to create post'
+                msg: 'Failed to create post',
             });
         }
     }
 };
 
-module.exports.getPostByID = async ({params: id = null}, res) => {
+module.exports.getPostByID = async ({ params: id = null }, res) => {
     try {
-        const [post, user] = await post.getPostByID(id);
+        const [post, user] = await postModel.getPostByID(id);
         post.user_info = user;
         res.status(200).json({
             error: false,
-            data: post
+            data: post,
         });
     } catch (e) {
-        console.error(err);
+        console.error(e);
         res.status(404).json({
             error: true,
-            data: 'Failed to find post'
+            data: 'Failed to find post',
         });
     }
 };
 
 module.exports.search = async (req, res) => {
-    let {page, limit, sort, tags} = req.query;
+    let { page } = req.query;
+    const { limit, sort, tags } = req.query;
 
     if (page) {
         page = Number(page);
         req.query.offset = 0;
+        // eslint-disable-next-line no-restricted-globals
         if (!isNaN(page)) {
             req.query.offset = Math.floor(page * limit);
         }
         delete req.query.page;
     }
 
-    if (limit)
+    if (limit) {
         req.query.limit = Number(limit);
+    }
 
-    if (tags)
+    if (tags) {
         req.query.tags = tags instanceof Array ? tags : [tags];
+    }
 
     req.query.sort = sort ? JSON.parse(sort) : {};
 
     try {
-        const posts = await post.search(req.query);
+        const posts = await postModel.search(req.query);
         res.status(200).json({
             error: false,
-            data: posts
+            data: posts,
         });
     } catch (e) {
         console.error(e);
         res.status(500).json({
             error: true,
-            msg: 'Search failed'
+            msg: 'Search failed',
         });
     }
 };
 
-module.exports.delete = async ({body: {post = null}}, res) => {
+module.exports.delete = async ({ body: { post = null } }, res) => {
     try {
-        await post.delete(post);
+        await postModel.delete(post);
+        res.status(200).json({
+            error: false,
+            data: null,
+        });
     } catch (e) {
         console.error(e);
         res.status(404).json({
-           error: true,
-           msg: 'Failed to delete post'
+            error: true,
+            msg: 'Failed to delete post',
         });
     }
 };
--- model/posts_model.js
+++ model/posts_model.js
@@ -1,40 +1,46 @@
 const config = require('../config');
 const db = require('pg-bricks').configure(config[process.env.NODE_ENV || 'development']);
-const Rx = require('rx');
 
-module.exports.addPosts = (description, url, tags, owner) => db.insert('posts', {description, 'image_url': url, 'user_id': owner}).returning('*').row();
+module.exports.addPosts = (description, url, tags, owner) => db.insert('posts', { description, image_url: url, user_id: owner }).returning('*').row();
 
-module.exports.getPostByID = ({id}) => {
-    const getPost = db.select('p.id AS post_id', 'p.description', 'p.image_url').from('posts as p').join('users u').on('p.user_id', 'u.id').where({'p.id': id}).row();
+module.exports.getPostByID = ({ id }) => {
+    const getPost = db.select('p.id AS post_id', 'p.description', 'p.image_url').from('posts as p').join('users u').on('p.user_id', 'u.id')
+        .where({ 'p.id': id })
+        .row();
 
-    const getUserInfo = db.select('u.id as user_id', 'u.profile_picture_url').from('users as u').join('posts p').on('u.id', 'p.user_id').where({'p.id': id}).row();
+    const getUserInfo = db.select('u.id as user_id', 'u.profile_picture_url').from('users as u').join('posts p').on('u.id', 'p.user_id')
+        .where({ 'p.id': id })
+        .row();
 
     return Promise.all([getPost, getUserInfo]);
 };
 
 // TODO: add sort
-module.exports.search = async ({tags, offset = 0, limit = 15, sort, description = ""}) => {
-    let findParams = {};
-    if (tags)
-        findParams.tags = tags;
-    if (description)
-        findParams.description = description;
+module.exports.search = async ({
+    tags, offset = 0, limit = 15, description = '',
+}) => {
+    const findParams = {};
+    if (tags) { findParams.tags = tags; }
+    if (description) { findParams.description = description; }
 
     const getPosts = async (postIDs) => {
         const hasIDs = postIDs !== undefined;
         const postIDQuery = hasIDs ? ' AND P.id IN $4' : '';
-        let params = [`%${description}%`, offset, limit];
-        if (hasIDs)
-            params.push(postIDs);
+        const params = [`%${description}%`, offset, limit];
+        if (hasIDs) { params.push(postIDs); }
 
         return db.raw(`SELECT P.id, P.description, P.image_url, U.id as user_id, U.username FROM POSTS as P, USERS as U WHERE P.USER_ID = U.ID AND P.DESCRIPTION LIKE $1${postIDQuery} ORDER BY P.id DESC OFFSET $2 LIMIT $3`, params).rows();
     };
 
-    const postIDs = tags !== undefined && tags.length > 0 ? await db.select('pt.post_id').from('post_tags as pt').join('tags t').on('t.id', 'pt.tag_id').where($in('t.name', tags)).rows() : undefined;
+    const postIDs = tags !== undefined && tags.length > 0 ? await db.select('pt.post_id').from('post_tags as pt').join('tags t').on('t.id', 'pt.tag_id')
+        // eslint-disable-next-line no-undef
+        .where($in('t.name', tags))
+        .rows() : undefined;
 
     return getPosts(postIDs);
 };
 
-module.exports.delete = (id) => db.delete().from('posts').where({id}).run();
+module.exports.delete = id => db.delete().from('posts').where({ id }).run();
 
-module.exports.batchDelete = (postsToDelete) => db.delete().from('posts').where($in('id', postsToDelete)).run();
+// eslint-disable-next-line no-undef
+module.exports.batchDelete = postsToDelete => db.delete().from('posts').where($in('id', postsToDelete)).run();
